543ada468efada7d0f27ee6172c505e24e9a09bd
Samuel Siahaan <samysiahaaan@gmail.com>
2023-06-26 11:22:24 +0700

    update footer

--- resources/views/theme/web/footer.blade.php
+++ resources/views/theme/web/footer.blade.php
@@ -3,7 +3,7 @@
 
         <div class="footer pt-20 pb-45">
             <div class="row">
-                <div class="col-lg-3 col-md-9">
+                <div class="col-lg-3 col-md-6 col-sm-12">
                     <div class="footer-about pt-30">
                         <a href="#"><img src="{{ asset('images/Logo_Mpp.png') }}" alt="logo"></a>
                         <p class="text-white">Donec vel ligula ornare, finibus ex at, vive risus. Aenean velit ex, finibus elementum
@@ -11,9 +11,9 @@
                         <span><i class="fa fa-globe"></i>devdpmptsp.pemkomedan.go.id</span>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-9">
+                <div class="col-lg-4 col-md-6 col-sm-12">
                     <div class="footer-title pt-30">
-                        <h5>Get In Tuch</h5>
+                        <h5 class="text-white">Informasi</h5>
                     </div>
                     <div class="footer-address">
                         <ul>
@@ -52,6 +52,12 @@
                             </li>
                         </ul>
                     </div>
+                </div>
+                <div class="col-lg-5 col-md-6 col-sm-12">
+                    <div class="footer-title pt-30">
+                        <h5 class="text-white">Map</h5>
+                    </div>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3348.615168497924!2d98.67255112627613!3d3.5419774911033564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3031300853fb9103%3A0x6414a8d0bb1e2059!2sDinas%20Penanaman%20Modal%20dan%20Pelayanan%20Terpadu%20Satu%20Pintu%20(DPMPTSP)%20Kota%20Medan!5e0!3m2!1sid!2sid!4v1687753170633!5m2!1sid!2sid" width="100%" height="80%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
