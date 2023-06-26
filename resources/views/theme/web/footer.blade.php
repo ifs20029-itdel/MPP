4ea3d12d59aca088db866f0e76aa9d9e39ff5248
ifs20029-itdel <ifs20029@students.del.ac.id>
2023-06-26 11:03:49 +0700

    update

--- resources/views/theme/web/footer.blade.php
+++ resources/views/theme/web/footer.blade.php
@@ -8,10 +8,10 @@
                         <a href="#"><img src="{{ asset('images/Logo_Mpp.png') }}" alt="logo"></a>
                         <p class="text-white">Donec vel ligula ornare, finibus ex at, vive risus. Aenean velit ex, finibus elementum
                             eu, dignissim varius augue. </p>
-                        <span><i class="fa fa-globe"></i>www.email@gmail.com</span>
+                        <span><i class="fa fa-globe"></i>devdpmptsp.pemkomedan.go.id</span>
                     </div>
                 </div>
-                <div class="col-lg-6 col-md-6 col-sm-9">
+                <div class="col-lg-4 col-md-6 col-sm-9">
                     <div class="footer-title pt-30">
                         <h5>Get In Tuch</h5>
                     </div>
@@ -22,8 +22,8 @@
                                     <i class="fa fa-map-marker"></i>
                                 </div>
                                 <div class="address">
-                                    <h5>Fresh Water</h5>
-                                    <p>45 Grand Central Terminal <br> New York.</p>
+                                    <h5>Dinas Penanaman Modal dan Pelayanan Terpadu Satu Pintu</h5>
+                                    <p>Jl. Jenderal Besar A.H. Nasution No.32 <br> Kota Medan.</p>
                                 </div>
                             </li>
                             <li>
@@ -31,7 +31,7 @@
                                     <i class="fa fa-volume-control-phone"></i>
                                 </div>
                                 <div class="address">
-                                    <p>+00123654789</p>
+                                    <p>+62 822-7773-3130</p>
                                 </div>
                             </li>
                             <li>
@@ -39,7 +39,15 @@
                                     <i class="fa fa-envelope-o"></i>
                                 </div>
                                 <div class="address">
-                                    <p>freshwater@gmail.com</p>
+                                    <p>dpmptsp@pemkomedan.go.id</p>
+                                </div>
+                            </li>
+                            <li>
+                            <div class="icon">
+                                    <i class="fa fa-envelope-o"></i>
+                                </div>
+                                <div class="address">
+                                    <p>dpmp.kota.medan@oss.go.id</p>
                                 </div>
                             </li>
                         </ul>
@@ -50,7 +58,7 @@
     </div>
 
     <div class="copyright pt-15 pb-15 text-center">
-        <p>&copy; All Rights Reserved <span>Fresh Water</span> 2020.</p>
+        <p>&copy; All Rights Reserved <span>DPMPTSP</span> 2023.</p>
     </div>
 
 </footer>
