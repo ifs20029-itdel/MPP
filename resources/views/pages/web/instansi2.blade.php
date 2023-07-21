e3fe60669a0734f6bffc7bd6843d674e6510822d
Samuel Siahaan <samysiahaaan@gmail.com>
2023-07-21 11:07:30 +0700

    update

--- resources/views/pages/web/instansi2.blade.php
+++ resources/views/pages/web/instansi2.blade.php
@@ -2,12 +2,12 @@
     <!--====== SLIDER PART START ======-->
     <li></li>
     <li></li>
-   
+    <li></li>
     <!--====== SLIDER PART ENDS ======--> 
 
     <!--====== PRODUCTS PART START ======-->
 
-    <section id="products-part" class="pt-65">
+    <section id="products-part" class="pt-65 mb-100">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-lg-8">
@@ -23,50 +23,16 @@
                     </div>
                 </div>
             </div>
-            <section id="checkout-part" class="pt-25">
+        </div>
         <div class="container">
             <div class="row">
                 <div class="col-lg-6">
                     <div class="checkout-box mt-50">
                         <div class="title">
-                            <h5>Buyer Info</h5>
-                            <h5 class="float-right">Login Here</h5>
+                            <h5>Nama Instansi</h5>
                         </div>
                         <div class="buyer-info">
-                            <form action="#">
-                                <div class="form-box">
-                                    <label>First Name :</label>
-                                    <input type="text">
-                                </div>
-                                <div class="form-box">
-                                    <label>Last Name :</label>
-                                    <input type="text">
-                                </div>
-                                <div class="form-box">
-                                    <label>Address 1 :</label>
-                                    <input type="text">
-                                </div>
-                                <div class="form-box">
-                                    <label>Address 2 :</label>
-                                    <input type="text">
-                                </div>
-                                <div class="form-box">
-                                    <label>City :</label>
-                                    <input type="text">
-                                </div>
-                                <div class="form-box">
-                                    <label>Postal Code :</label>
-                                    <input type="text">
-                                </div>
-                                <div class="form-box">
-                                    <label>Country :</label>
-                                    <input type="text">
-                                </div>
-                                <div class="form-box">
-                                    <label>State :</label>
-                                    <input type="text">
-                                </div>
-                            </form>
+                            <img src="images/Polresta.png" alt="Polresta"> 
                         </div>
                     </div>
                 </div>
@@ -75,45 +41,66 @@
                         <div class="details">
                             <div class="order-list">
                                 <ul class="features-list page-margin-top-section">
-                                <li class="gray">
-                                    <div class="icon template-location1"></div>
-                                    <div class="item-content">
-                                        <p>Alamat</p>
-                                        <h5>Medan Johor</h5>
-                                    </div>
-                                        </li>
-                                <li class="gray">
-                                    <div class="icon template-location1"></div>
-                                    <div class="item-content">
-                                        <p>Website</p>
-                                        <h5>-</h5>
-                                    </div>
-                                </li>
-                                <li class="gray">
-                                    <div class="icon template-location1"></div>
-                                    <div class="item-content">
-                                        <p>Email</p>
-                                        <h5>-</h5>
-                                    </div>
-                                </li>
-                                <li class="gray">
-                                    <div class="icon template-location1"></div>
-                                    <div class="item-content">
-                                        <p>Phone</p>
-                                        <h5>-</h5>
-                                    </div>
-                                </li>
+                                    <li class="gray">
+                                        <div class="icon template-location1"></div>
+                                        <div class="item-content">
+                                            <p>Alamat</p>
+                                            <h5>Medan Johor</h5>
+                                        </div>
+                                    </li>
+                                    <li class="gray">
+                                        <div class="icon template-location1"></div>
+                                        <div class="item-content">
+                                            <p>Website</p>
+                                            <h5>-</h5>
+                                        </div>
+                                    </li>
+                                    <li class="gray">
+                                        <div class="icon template-location1"></div>
+                                        <div class="item-content">
+                                            <p>Email</p>
+                                            <h5>-</h5>
+                                        </div>
+                                    </li>
+                                    <li class="gray">
+                                        <div class="icon template-location1"></div>
+                                        <div class="item-content">
+                                            <p>Phone</p>
+                                            <h5>-</h5>
+                                        </div>
+                                    </li>
                                 </ul>
                             </div>
                         </div>
                     </div>
-                    
-   
-            
-    </section>
-    <!--====== PRODUCTS PART ENDS ======-->
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="checkout-box mt-50">
+                        <div class="title">
+                            <h5>Layanan Instansi</h5>
+                        </div>
+                        <div class="m-5">
+                            <div class="singel-services">
+                                <div class="services-cont">
+                                    <a href="#" class="m-1">Pengaduan SIM <span><i class="fa fa-long-arrow-right"></i></span></a>
+                                    <a href="#" class="m-1">Pengaduan Kehilangan <span><i class="fa fa-long-arrow-right"></i></span></a>
+                                    <a href="#" class="m-1">Pengaduan Kehilangan <span><i class="fa fa-long-arrow-right"></i></span></a>
+                                    <a href="#" class="m-1">Pengaduan Kehilangan <span><i class="fa fa-long-arrow-right"></i></span></a>
+                                    <a href="#" class="m-1">Pengaduan Kehilangan <span><i class="fa fa-long-arrow-right"></i></span></a>
+                                    <a href="#" class="m-1">Pengaduan Kehilangan <span><i class="fa fa-long-arrow-right"></i></span></a>
+                                </div>
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> 
 
 
-    <!--====== SERVICES PART ENDS ======-->
 
 </x-WebLayout>
