ea8e0fe2fab8af55a2085ba7d0867f20c2dce8cd
ifs20029-itdel <ifs20029@students.del.ac.id>
2023-07-24 08:51:38 +0700

    update

--- resources/views/theme/web/footer.blade.php
+++ resources/views/theme/web/footer.blade.php
@@ -8,6 +8,7 @@
                         <a href="#"><img src="{{ asset('images/Logo_Mpp.png') }}" alt="logo"></a>
                         <p class="text-white">Melayani dengan Cepat dan Transparan. </p>
                         <span><i class="fa fa-globe"></i><a href="https://devdpmptsp.pemkomedan.go.id/dpmptspwebaplikasi/">devdpmptsp.pemkomedan.go.id</a> </span>
+                        <span><i></i><a href="{{route('admin')}}">Login</a> </span>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-12">
